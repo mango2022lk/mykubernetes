475c11ea176b4886226506651ed2863c1c76e189
likai <542712370@qq.com>
2023-02-05 17:03:38 +0800

    课程8练习 httpserver上云实践

---
+++ module8/main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"io"
+	"net"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+	"time"
+)
+
+var logLevel string
+var viperInstance *viper.Viper
+
+func main() {
+	httpserverConf := os.Getenv("HTTPSERVER_CONF")
+	log.Info("configFile from env is " + httpserverConf)
+	if httpserverConf == "" {
+		httpserverConf = "/etc/httpserver/httpserver.properties"
+	}
+	log.Info("confFile is " + httpserverConf)
+	viperInstance = viper.New()                 // viper实例
+	viperInstance.SetConfigFile(httpserverConf) // 指定配置文件路径
+
+	err := viperInstance.ReadInConfig()
+	if err != nil { // 处理读取配置文件的错误
+		panic(fmt.Errorf("Fatal error config file: %s \n", err))
+	}
+	viperInstance.WatchConfig()
+	viperInstance.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Detect config change: %s \n", e.String())
+		log.Warn("Config file updated.")
+		viperLoadConf(viperInstance) // 加载配置的方法
+	})
+	viperLoadConf(viperInstance)
+
+	port := 8080
+	portstr := ":" + strconv.Itoa(port)
+	log.Info("httpserver listend port: ", port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", HttpHandler)
+	mux.HandleFunc("/healthz", HealthZ)
+	mux.HandleFunc("/sleep", SleepTest)
+	server := &http.Server{
+		Addr:    portstr,
+		Handler: mux,
+	}
+	go server.ListenAndServe()
+	listenSignal(context.Background(), server)
+}
+
+func dynamicConfig() {
+	viperInstance.WatchConfig()
+	viperInstance.OnConfigChange(func(e fsnotify.Event) {
+		fmt.Println("Detect config change: %s \n", e.String())
+		log.Warn("Config file updated.")
+		viperLoadConf(viperInstance) // 加载配置的方法
+	})
+}
+
+func viperLoadConf(viperInstance *viper.Viper) {
+	logLevel = viperInstance.GetString("log_level")
+	level, err := log.ParseLevel(logLevel)
+	if err != nil {
+		level = log.GetLevel()
+	}
+	log.SetLevel(level)
+	myconf := viperInstance.GetString("my_conf")
+	log.Trace(myconf + " in viperLoadConf")
+	log.Debug(myconf + " in viperLoadConf")
+	log.Info(myconf + " in viperLoadConf")
+	log.Warn(myconf + " in viperLoadConf")
+	log.Error(myconf + " in viperLoadConf")
+}
+
+func HealthZ(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
+func HttpHandler(w http.ResponseWriter, r *http.Request) {
+	myconf := viperInstance.GetString("my_conf")
+	log.Trace(myconf)
+	log.Debug(myconf)
+	log.Info(myconf)
+	log.Warn(myconf)
+	log.Error(myconf)
+	ip := ClientIP(r)
+	httpcode := 200
+	log.Info("reqLog: clientIP : " + ip + " httpcode " + strconv.Itoa(httpcode))
+	headers := r.Header
+	log.Info("req headers : %+v", headers)
+	for k, v := range headers {
+		for _, val := range v {
+			w.Header().Set(k, val)
+		}
+	}
+	version := os.Getenv("VERSION")
+	if version != "" {
+		w.Header().Set("VERSION", version)
+	}
+	w.WriteHeader(httpcode)
+	io.WriteString(w, "ok")
+}
+
+func ClientIP(r *http.Request) string {
+	xForwardedFor := r.Header.Get("X-Forwarded-For")
+	ip := strings.TrimSpace(strings.Split(xForwardedFor, ",")[0])
+	if ip != "" {
+		return ip
+	}
+	ip = strings.TrimSpace(r.Header.Get("X-Real-Ip"))
+	if ip != "" {
+		return ip
+	}
+	if ip, _, err := net.SplitHostPort(strings.TrimSpace(r.RemoteAddr)); err == nil {
+		return ip
+	}
+	return ""
+}
+
+func SleepTest(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	sleeptimeStr := values.Get("sleep")
+	sleeptime, err := strconv.Atoi(sleeptimeStr)
+	if err != nil {
+		sleeptime = 1
+	}
+	time.Sleep(time.Duration(sleeptime) * time.Second)
+	fmt.Fprintln(w, "Hello world, sleep "+strconv.Itoa(sleeptime)+"s")
+	log.Printf("Hello world, sleep %+vs", sleeptime)
+}
+
+// 优雅终止
+func listenSignal(ctx context.Context, httpSrv *http.Server) {
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+
+	select {
+	case <-sigs:
+		fmt.Println("notify sigs")
+		httpSrv.Shutdown(ctx)
+		fmt.Println("http shutdown gracefully")
+	}
+}
